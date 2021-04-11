d9a2ee849da541cadbc42f28725f09489881376a
Darren Phillips <dzp0067@auburn.edu>
2021-04-11 17:39:08 -0500

    Updated formatting

--- M6Activity/Form1.cs
+++ M6Activity/Form1.cs
@@ -26,7 +26,7 @@ namespace M6Activity
             obj.shift = Convert.ToInt32(shiftBox.Text);
             obj.payRate = Convert.ToInt32(payBox.Text);
 
-            MessageBox.Show("Employee Info \n" +
+            MessageBox.Show("Employee Info \n\n" +
                             "Name: " + obj.empName + " Employee ID#: " + obj.empNumber + " Employee Shift Number: " + obj.shift + " Employee Pay Rate: " + obj.payRate);
         }
     }
